ff15f9df0d41e3c1b7182e2272e708382530d1b8
Jakub Jirous <info@jakubjirous.cz>
2021-11-01 13:19:10 -0700

    [ui] basic components and basic styling

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,20 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "prettier",
+  ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/html-quotes": ["error", "double", { avoidEscape: false }],
+    quotes: ["error", "single", { avoidEscape: false }],
   },
 };
---
+++ src/components/learning-resources/StoredResource.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="(resource, index) in resources"
+      :key="index"
+      :description="resource.description"
+      :link="resource.link"
+      :title="resource.title"
+    >
+    </learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource';
+
+export default {
+  name: 'StoredResource',
+  components: {
+    'learning-resource': LearningResource,
+  },
+  props: {
+    resources: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0 auto;
+  padding: 0;
+  max-width: 40rem;
+}
+</style>
